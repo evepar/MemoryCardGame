f48bc453727b204536f18eb9b3dd48ef9660cce7
Eve <eveliinaparanko@Macintosh-6.local>
2017-10-04 18:40:02 +0100

    Deck and shuffle
    changed cards into blanks and shuffle works

--- js/app.js
+++ js/app.js
@@ -6,8 +6,10 @@
  * "card match"
  * "card open nomatch"
  */
-const deck = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt",
-              "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb"];
+const initialCards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt",
+              "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+
+const shakeDeck = shuffle(initialCards);
 
 /*
  * Display the cards on the page
