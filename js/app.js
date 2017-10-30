e8720155521d9029d8da6677cdf3c3439fb92b9b
Eve <eveliinaparanko@Macintosh-6.local>
2017-10-30 19:27:06 +0000

    Adding a timer button

--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@ const initialCards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-
 
 let shakeDeck = shuffle(initialCards);
 
+// reshuffle the deck https://stackoverflow.com/questions/6653556/jquery-javascript-function-to-clear-all-the-fields-of-a-form
+// button is class "restart" in HTML
+$('#myForm').trigger("reset");
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -43,23 +47,27 @@ $(".card").click(function(){
   const $card = $(this)
   // check if card does not have the class open/ show Karol
   // before adding open/ show Karol
-  // jQuey hasClass method Karol
+  // jQuery hasClass method Karol
   // check if the compare array's length is less than 2 Karol
-  $card.addClass("open show");
-  if (!$compare) {
-    $compare = $card;
-  } else if ($compare[0].innerHTML === $card[0].innerHTML) {
-    console.log("match");
-    $card.addClass("match");
-    $compare.addClass("match");
-    $compare = null;
-  } else {
-    console.log("doesn't match");
-    setTimeout(function(){
-      $card.removeClass("open show");
-      $compare.removeClass("open show");
+  if $(card:not(:hasClass("open show"))) {
+    $card.addClass("open show");
+    if (!$compare) {
+      $compare = $card;
+    } else if ($compare[0].innerHTML === $card[0].innerHTML) {
+      console.log("match");
+      $card.addClass("match");
+      $compare.addClass("match");
       $compare = null;
-    },100)
+    } else {
+      console.log("doesn't match");
+      setTimeout(function(){
+        $card.removeClass("open show");
+        $compare.removeClass("open show");
+        $compare = null;
+      },100)
+  } else {
+    $card.attr('disabled', true);
+  }
   }
 });
 
@@ -67,11 +75,25 @@ let $compare = null
 
 
 // timer https://stackoverflow.com/questions/2604450/how-to-create-a-jquery-clock-timer#answer-19744442
+// and end timer when game is won
+ $(document).ready(function(){
+         function getdate(){
+                var today = new Date();
+            var h = today.getHours();
+            var m = today.getMinutes();
+            var s = today.getSeconds();
+             if(s<10){
+                 s = "0"+s;
+             }
 
-// if match http://api.jquery.com/animate/ https://www.w3schools.com/jquery/jquery_animate.asp
-// or change color to green for a moment
+            $("h1").text(h+" : "+m+" : "+s);
+             setTimeout(function(){getdate()}, 500);
+            }
 
-// if cards don't match, close them
+        $("button").click(getdate);
+    });
+
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -83,27 +105,3 @@ let $compare = null
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
-/*
-https://learn.jquery.com/about-jquery/how-jquery-works/
-
-A callback is a function that is passed as an argument to another function and is executed after its
-parent function has completed. with (inside a function) or without arguments.
-
-https://learn.jquery.com/using-jquery-core/faq/how-do-i-select-an-item-using-class-or-id/
-
-event listener for a card
-.bind()
-Attach a handler to an event for the elements.
-
-.click()
-Bind an event handler to the “click” JavaScript event, or trigger that event on an element.
-
-.show()
-Display the matched elements.
-
-Add to a list of open cards
-.add()
-Create a new jQuery object with elements added to the set of matched elements.
-Don’t know how to check the list of open cards for a match
-*/
