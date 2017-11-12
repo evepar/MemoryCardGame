4b226b3e518d3cb5ea5dcf4f4367751e15489dc0
Eve <eveliinaparanko@Macintosh-6.local>
2017-11-12 16:27:19 +0000

    Reset, modal
    Reset mostly works, modal does not

--- js/app.js
+++ js/app.js
@@ -36,50 +36,60 @@ function shuffle(array) {
     return array;
 }
 
-shakeDeck.forEach(function(card, index){
-  //console.log(card);
-  const cardElement = '<li class="card"><i class="fa ' + card + '"></i></li>'
-  // console.log(cardElement);
-  $(".deck").append(cardElement);
-})
+function makeDeck() {
+  $(".deck").empty()
+  shakeDeck.forEach(function(card, index){
+    //console.log(card);
+    const cardElement = '<li class="card"><i class="fa ' + card + '"></i></li>'
+    // console.log(cardElement);
+    $(".deck").append(cardElement);
+  })
+}
 
-$(".card").click(function(){
-  const $card = $(this)
-  if (!$card.hasClass("open show")) {
-    $card.addClass("open show");
-    if (!$compare) {
-      $compare = $card;
-    } else if ($compare[0].innerHTML === $card[0].innerHTML) {
-      console.log("match");
-      $card.addClass("match");
-      $compare.addClass("match");
-      $compare = null;
-      matchCount++;
-        if (matchCount === 8){
-        clearInterval(timer)
-        gameover()
-        }
-    } else {
-      console.log("doesn't match");
-      setTimeout(function(){
-        $card.removeClass("open show");
-        $compare.removeClass("open show");
+makeDeck();
+
+function playGame() {
+  $(".card").click(function(){
+    const $card = $(this)
+    if (!$card.hasClass("open show")) {
+      $card.addClass("open show");
+      if (!$compare) {
+        $compare = $card;
+      } else if ($compare[0].innerHTML === $card[0].innerHTML) {
+        console.log("match");
+        $card.addClass("match");
+        $compare.addClass("match");
         $compare = null;
-      },100)
-  }
-  } else {
-    $card.attr('disabled', true);
-  }
-});
+        matchCount++;
+          if (matchCount === 8){
+          clearInterval(timer)
+          $('.myModal').modal('show')
+          }
+      } else {
+        console.log("doesn't match");
+        setTimeout(function(){
+          $card.removeClass("open show");
+          $compare.removeClass("open show");
+          $compare = null;
+        },100)
+    }
+    } else {
+      $card.attr('disabled', true);
+    }
+  });
+}
+
+playGame();
 
 let $compare = null
 let matchCount = 0
 var timer
 // timer https://stackoverflow.com/questions/2604450/how-to-create-a-jquery-clock-timer#answer-19744442
 // and end timer when game is won
+function timeGame() {
  $(document).ready(function(){
          function getdate(){
-                var start = new Date().getTime()
+               var start = new Date().getTime()
                timer = setInterval(function(){
                var currentTime = new Date().getTime()
                var elapsedTime = currentTime - start
@@ -94,17 +104,25 @@ var timer
             }
         $("button").click(getdate);
     });
+}
+
+timeGame();
 
 //modal https://getbootstrap.com/docs/4.0/components/modal/
-function gameover() {
-  <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-sm">
-      <div class="modal-content">
-        "You win!"
-      </div>
-    </div>
-  </div>
+
+// reset page https://stackoverflow.com/questions/30347724/refresh-page-with-reset-button
+//$("#restart").click(function(){
+//  document.location.reload();
+//});
+
+function resetGame() {
+  clickCount = 0;
+  makeDeck();
+  playGame();
+  timeGame();
 }
+
+$('.restart').click(resetGame);
 
 /*
  * set up the event listener for a card. If a card is clicked:
