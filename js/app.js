4a5e2e0aa58ea3ec2f94fa6179f62bcdf9ad2809
Eve <eveliinaparanko@Macintosh-6.local>
2017-11-30 17:38:10 +0000

    Star rating 1-3

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ makeDeck();
 var clicks = 0;
 
 function lessStars(clicks) {
-    var starRating = [17,22,27];
+    var starRating = [17,22];
     var stars = starRating.length;
     $('.stars').children().each(function(i, starElem) {
     if (starRating[i] === clicks) $(starElem).addClass("star-lost")
